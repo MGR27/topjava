f47858501d01fbbf4e2dcd5af74d048537be09a2
MGR27 <java.rockm@gmail.com>
2021-08-22 15:00:02 +0300

    11 13 swagger2

--- src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
@@ -9,12 +9,13 @@ import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.View;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.to.MealTo;
+import springfox.documentation.annotations.ApiIgnore;
 
-import javax.validation.Valid;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
+@ApiIgnore
 @RestController
 @RequestMapping(value = "/profile/meals", produces = MediaType.APPLICATION_JSON_VALUE)
 public class MealUIController extends AbstractMealController {
@@ -27,7 +28,7 @@ public class MealUIController extends AbstractMealController {
     }
 
     @Override
-    @GetMapping( "/{id}")
+    @GetMapping("/{id}")
     @JsonView(View.JsonUI.class)
     public Meal get(@PathVariable int id) {
         return super.get(id);
